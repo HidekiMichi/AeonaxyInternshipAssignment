64f4ed74f910500d57d8bcfcd303819f04ac2381
Aatish <aatishkumarsingh30@gmail.com>
2024-04-07 23:18:23 +0530

    done

--- src/Components/SocialMedia.js
+++ src/Components/SocialMedia.js
@@ -8,11 +8,11 @@ import { FaPinterest } from "react-icons/fa";
 function SocialMedia(){
     return(
         <Card className="flex md:justify-start sm:justify-center">
-            <a ><TiSocialDribbble className="text-2xl mr-3"/></a>
-            <a ><TiSocialTwitter className="text-2xl mr-4"/></a>
-            <a ><FaFacebookSquare className="text-xl mr-4"/></a>
-            <a ><FaInstagram className="text-xl mr-4"/></a>
-            <a ><FaPinterest className="text-lg"/></a>
+            <p><TiSocialDribbble className="text-2xl mr-3"/></p>
+            <p><TiSocialTwitter className="text-2xl mr-4"/></p>
+            <p><FaFacebookSquare className="text-xl mr-4"/></p>
+            <p><FaInstagram className="text-xl mr-4"/></p>
+            <p><FaPinterest className="text-lg"/></p>
         </Card>
     );
 }
